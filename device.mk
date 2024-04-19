6bb06d3826c008e4b72a7859182e0779b7cd9e00
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:33:43 +0000

    Adapt to LMODroid by device-add script

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/xiaomi/natrium/natrium-vendor.mk)
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 # Permissions
 PRODUCT_COPY_FILES += \
